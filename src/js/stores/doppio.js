ee87ebaf9bf955720a46f0b80b01269d461c2157
Niklas Lindgren <nikc@iki.fi>
2015-08-09 21:05:21 +0300

    Add action logging middleware

--- src/js/stores/doppio.js
+++ src/js/stores/doppio.js
@@ -1,14 +1,22 @@
-import { combineReducers, createStore } from "redux";
+import debug from "debug";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import { sessions } from "../reducers/session";
 import { profile } from "../reducers/profile";
+
+const log = debug("doppio:stores:doppio");
 
 let initialState = {
     sessions: [],
     profile: null
 };
 
+let logger = store => next => action => {
+    log("dispatching", action);
+    return next(action);
+};
+
 let doppioApp = combineReducers({ sessions, profile });
 
 export function getStore(state = initialState) {
-    return createStore(doppioApp, state);
+    return applyMiddleware(logger)(createStore)(doppioApp, state);
 }
